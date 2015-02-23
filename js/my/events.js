871d8708c27520f226447ec3f2a33cf3553abc63
Adrian Chen <adrianchen@Adrians-MacBook-Pro.local>
2015-02-23 01:36:53 -0700

    Finished 50/50

--- js/my/events.js
+++ js/my/events.js
@@ -3,5 +3,13 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+    console.log('listing events');
+
+    $.get("https://api.github.com/users/briannewsom/events", github, function(events){
+        $.get("/git-jquery/templates/eventList.jade", function(template){
+            var html = jade.render(template, {items: events})
+            $('#list').html(html)
+        })
+    })
+
 }
--- js/my/repo.js
+++ js/my/repo.js
@@ -3,5 +3,14 @@ function myRepo(full_name){
     // display a particular repo of mine in #details
     // e.g., https://api.github.com/repos/doubleshow/up
 
-    alert('todo')
+    console.log('viewing repo')
+
+    $.get("https://api.github.com/repos/briannewsom/" + full_name, github, function(data) {
+        var repo = data
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
+            console.log(repo);
+            var html = jade.render(template, {items: repo})
+            $("#detail").html('hi')
+        })
+    })
 }
