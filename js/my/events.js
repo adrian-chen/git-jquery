ca9a957195243389428ba69f1a980e39bbb2e715
Adrian Chen <adrianchen@Adrians-MacBook-Pro.local>
2015-02-23 01:37:29 -0700

    build

--- js/my/events.js
+++ js/my/events.js
@@ -3,13 +3,10 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    console.log('listing events');
-
-    $.get("https://api.github.com/users/briannewsom/events", github, function(events){
-        $.get("/git-jquery/templates/eventList.jade", function(template){
-            var html = jade.render(template, {items: events})
-            $('#list').html(html)
-        })
+    $.get("https://api.github.com/users/adrian-chen/events", github, function(events){
+    	$.get("/git-jquery/templates/eventList.jade", function(template){
+    		var html = jade.render(template, {items: events})
+    		$('#list').html(html)
+    	})
     })
-
 }
--- js/my/repo.js
+++ js/my/repo.js
@@ -3,14 +3,11 @@ function myRepo(full_name){
     // display a particular repo of mine in #details
     // e.g., https://api.github.com/repos/doubleshow/up
 
-    console.log('viewing repo')
-
-    $.get("https://api.github.com/repos/briannewsom/" + full_name, github, function(data) {
-        var repo = data
-        $.get("/git-jquery/templates/repoList.jade", function(template) {
-            console.log(repo);
-            var html = jade.render(template, {items: repo})
-            $("#detail").html('hi')
-        })
+    $.get("https://api.github.com/repos/" + full_name, github, function(data) {
+    	var repo = data;
+    	$.get("/git-jquery/templates/repoView.jade", function(template) {
+    		var html = jade.render(template, {item: repo})
+    		$("#details").html(html)
+    	})
     })
 }
