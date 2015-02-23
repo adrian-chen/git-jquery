d70bf244551849155954980bff9fdc3f407da3ea
Adrian Chen <adrianchen@Adrians-MacBook-Pro.local>
2015-02-23 01:36:53 -0700

    Finished 50/50

--- contents/js/my/events.js
+++ contents/js/my/events.js
@@ -3,5 +3,10 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+    $.get("https://api.github.com/users/adrian-chen/events", github, function(events){
+    	$.get("/git-jquery/templates/eventList.jade", function(template){
+    		var html = jade.render(template, {items: events})
+    		$('#list').html(html)
+    	})
+    })
 }
--- contents/js/my/repos.js
+++ contents/js/my/repos.js
@@ -3,5 +3,15 @@ function myRepos(){
     // display my repos in #list
     // e.g., https://api.github.com/users/doubleshow/repos
 
-    alert('todo')
+    console.log('listing repos')
+    $.get("https://api.github.com/users/adrian-chen/repos", github, function(data) {
+ 		 
+        var repos = data
+       $.get("/git-jquery/templates/repoList.jade", function(template) {
+            var html = jade.render(template, {items: repos})
+            $("#list").html(html)
+            myRepo(repos[0].full_name)
+        })
+    })
+
 }
